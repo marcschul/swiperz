52534b204970d601dfae4a13ccbbd3b691b50896
Marc Schulmeister <marcschulm@gmail.com>
2022-04-16 15:09:23 -0700

    update css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(props) {
 
   return (
     <div className="flex-col font-paint flex justify-center">
-    <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 pt-10">SWIPERZ</h1>
+    <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">SWIPERZ</h1>
       <div className="flex justify-center text-2xl">
         <h2 className="p-2 text-red-500">{player1}</h2>
         <h2 className="p-2 font-extrabold text-slate-600"> VS </h2>
