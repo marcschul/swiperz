a2484f89f93b35c0cf00e415c78a69e49d0b8cfa
Marc Schulmeister <marcschulm@gmail.com>
2022-04-16 19:57:41 -0700

    replace input for buttons, RB works correctly

--- src/components/Board/Grid.jsx
+++ src/components/Board/Grid.jsx
@@ -18,16 +18,17 @@ export default function Grid(props) {
   }
 
   return (
-    <div className="flex justify-center text-slate-300 ">
-      <table>
+      <table className="flex justify-center text-slate-300">
         <tbody>
           {grid.map((item, index) => {
             return (
-              <tr key={`${index}`}>
+              <tr key={`${index}`} className="max-h-[20px]">
                 
                 {/* LEFT BUTTON */}
                 <td key={`${index + '0'}`} 
-                    className="border-2" onClick={() => {
+                    className="font-paint text-slate-700 text-4xl p-0 m-0">
+                    <button 
+                    onClick={() => {
                     for (let i = 0; i < props.gameState.board[index].length; i++) {
                       if (!props.gameState.board[index][i]) {
                         const newBoard = [...props.gameState.board];
@@ -37,7 +38,6 @@ export default function Grid(props) {
                         if (props.gameState.currentPlayer === 1) {
                           newPlayer = 2;
                         }
-
                         if (props.gameState.currentPlayer === 2) {
                           newPlayer = 1;
                         }
@@ -51,7 +51,9 @@ export default function Grid(props) {
                       }
                     }
                     console.log(props.gameState)
-                  }}>LB</td>
+                  }}>+
+                  </button>
+                  </td>
 
                 {/* ROW */}
                 <td key={`${index + '1'}`} >{item[0]}</td>
@@ -64,15 +66,40 @@ export default function Grid(props) {
 
 
                 {/* RIGHT BUTTON */}
-                <td key={`${index + '8'}`}  className="border-2" onClick={() => {
-                    console.log('right button click', 'index ===', index); 
-                  }}>RB</td>
+                <td key={`${index + '9'}`} 
+                    className="font-paint text-slate-700 text-4xl p-0 m-0">
+                    <button 
+                    onClick={() => {
+                    for (let i = props.gameState.board[index].length - 1; i >= 0; i--) {
+                      if (!props.gameState.board[index][i]) {
+                        const newBoard = [...props.gameState.board];
+                        newBoard[index][i] = props.gameState.currentPlayer;
+
+                        let newPlayer;
+                        if (props.gameState.currentPlayer === 1) {
+                          newPlayer = 2;
+                        }
+                        if (props.gameState.currentPlayer === 2) {
+                          newPlayer = 1;
+                        }
+                        
+                        props.setGameState(prevState => ({
+                          ...prevState,
+                          board: newBoard,
+                          currentPlayer: newPlayer
+                        }))
+                        break;
+                      }
+                    }
+                    console.log(props.gameState)
+                  }}>+
+                  </button>
+                  </td>
 
               </tr>
             );
           })}
         </tbody>
       </table>
-    </div>
   );
 }
--- src/components/Board/Node.jsx
+++ src/components/Board/Node.jsx
@@ -1,19 +1,17 @@
-import { useEffect, useState } from 'react'
-
 export default function Block(props) {
 
   let classState;
   
   if (props.state === '0') {
-    classState = "bg-white m-1 min-w-[25px] min-h-[25px]";
+    classState = "bg-white mx-1 min-w-[25px] min-h-[25px]";
   }
 
   if (props.state === '1') {
-    classState = "bg-red-500 m-1 min-w-[25px] min-h-[25px]";
+    classState = "bg-red-500 mx-1 min-w-[25px] min-h-[25px]";
   }
 
   if (props.state === '2') {
-    classState = "bg-green-500 m-1 min-w-[25px] min-h-[25px]";
+    classState = "bg-green-500 mx-1 min-w-[25px] min-h-[25px]";
   }
 
   return (
