a48aa60ea5dbd8ec7cbd6b5e48250e22f7015308
Marc Schulmeister <marcschulm@gmail.com>
2022-05-24 15:00:50 -0700

    change 'new game' to 'loading..'

--- src/components/NewGame.jsx
+++ src/components/NewGame.jsx
@@ -1,41 +1,44 @@
-import axios from 'axios';
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-
 export default function NewGame(props) {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-  
   const setGameState = props.setGameState;
   const setAppState = props.setAppState;
 
   return (
     <div className="text-white">
-      <button onClick={() => {
-        localStorage.clear();
-        localStorage.setItem('player1', 1)
-        
-        axios.post('/games', {})
-        .then(function (response) {
-          const gameId = response.data[0]
-          navigate(`game/${gameId}`);
-          setAppState(prevState => ({
-            ...prevState,
-            player1: response.data[1],
-            player2: response.data[2],
-            currentPlayer: JSON.parse(response.data[3]),
-            gameOver: JSON.parse(response.data[4]),
-            message: response.data[5],
-          }));
-          setGameState(prevState => ({
-            ...prevState,
-            board: response.data[6],
-          }));
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-      }}>New Game ?</button>
+      <button
+        onClick={() => {
+          localStorage.clear();
+          localStorage.setItem("player1", 1);
+
+          axios
+            .post("/games", {})
+            .then(function (response) {
+              const gameId = response.data[0];
+              navigate(`game/${gameId}`);
+              setAppState((prevState) => ({
+                ...prevState,
+                player1: response.data[1],
+                player2: response.data[2],
+                currentPlayer: JSON.parse(response.data[3]),
+                gameOver: JSON.parse(response.data[4]),
+                message: response.data[5],
+              }));
+              setGameState((prevState) => ({
+                ...prevState,
+                board: response.data[6],
+              }));
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        }}
+      >
+        Loading...
+      </button>
     </div>
-  )
+  );
 }
